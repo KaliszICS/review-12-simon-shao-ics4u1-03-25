e520321589f67449baf1daae2cb6380de9b05a79
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-25 18:39:53 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,42 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.print(getAge(new String[]{"Rachel"}, new int[]{24}, "Rachel"));
+	}
+public static boolean isPalindrome(String word){
+
+word = word.replace(" ", "");
+word = word.toLowerCase();
+for (int i = 0; i<word.length(); i++){
+	if (word.charAt(i)!=(word.charAt(word.length()-1-i))){
+		return false;
+	}
+}
+return true;
+}
+
+public static int getAge(String[] names, int[] ages, String name){
+	int age = 0;
+	for (int i=0; i<names.length; i++){
+		if (names[i].equals(name)){
+age =  ages[i];
+		}
+	}
+return age;
+}
+
+public static int countWords(String word, char letter){
+	int count = 0;
+	String[] wordArr1 = word.split(" ");
+	for (int i = 0; i<wordArr1.length; i++){
+		for (int j = 0; j<wordArr1[i].length(); j++){
+					if (wordArr1[1].charAt(j)==letter){
+			count+=1;
+		}
+		}
 
 	}
+	return count;
+}
+
 }
